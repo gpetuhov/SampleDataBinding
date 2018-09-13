05ef4727f7b877d6e8b46f36cda843c396036803
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-09-13 14:55:10 +0300

    Add simple data binding

--- app/src/main/java/com/gpetuhov/android/sampledatabinding/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampledatabinding/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.gpetuhov.android.sampledatabinding
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gpetuhov.android.sampledatabinding.databinding.ActivityMainBinding
+import com.gpetuhov.android.sampledatabinding.model.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Set content view and get the binding
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Bind the user to the UI
+        binding.user = User("Bob")
     }
 }
---
+++ app/src/main/java/com/gpetuhov/android/sampledatabinding/model/User.kt
@@ -0,0 +1,3 @@
+package com.gpetuhov.android.sampledatabinding.model
+
+class User(val name: String)
