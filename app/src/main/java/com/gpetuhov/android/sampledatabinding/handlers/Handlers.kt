423a6bb2452c5dd0867e80c0f47d2fa4ed0988d7
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-09-13 15:55:56 +0300

    Handle clicks

--- app/src/main/java/com/gpetuhov/android/sampledatabinding/handlers/Handlers.kt
+++ app/src/main/java/com/gpetuhov/android/sampledatabinding/handlers/Handlers.kt
@@ -2,11 +2,19 @@ package com.gpetuhov.android.sampledatabinding.handlers
 
 import android.util.Log
 import android.view.View
+import com.gpetuhov.android.sampledatabinding.model.User
 import org.jetbrains.anko.toast
 
 class Handlers {
+
     fun onButtonClick(view: View) {
         Log.d("Handlers", "Button click")
         view.context.toast("Hello, world!")
+    }
+
+    // Long click listener must return boolean!
+    fun onButtonLongClick(view: View, user: User): Boolean {
+        view.context.toast("Hello, ${user.name}!")
+        return true
     }
 }
